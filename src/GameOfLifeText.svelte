cd83af7d4b6f60b56f8cb92c33822ef8a8900858
Justin Kumar <justinkumar007@gmail.com>
2021-04-13 17:06:46 -0500

    Added fps to the game of life simulation

--- src/GameOfLifeText.svelte
+++ src/GameOfLifeText.svelte
@@ -3,11 +3,45 @@ import { onMount } from 'svelte';
 
     let pre;
     let play_pause;
+    let framesPerSecond;
     import wasm from './alert/Cargo.toml';
 
     let universe;
 
     let animationId = null;
+    let frames = []
+    let lastFrameTimeStamp = performance.now();
+
+    const renderFPS = () => {
+        const now = performance.now();
+        const delta = now - lastFrameTimeStamp;
+        lastFrameTimeStamp = now;
+        const fps = 1 / delta * 1000;
+
+        // Save only the latest 100 timings.
+        frames.push(fps);
+        if (frames.length > 100) {
+            frames.shift();
+        }
+
+        // Find the max, min, and mean of our 100 latest timings.
+        let min = Infinity;
+        let max = -Infinity;
+        let sum = 0;
+        for (let i = 0; i < frames.length; i++) {
+            sum += frames[i];
+            min = Math.min(frames[i], min);
+            max = Math.max(frames[i], max);
+        }
+        let mean = sum / frames.length;
+
+        // Render the statistics.
+        framesPerSecond.textContent = `Frames per Second:
+                latest = ${Math.round(fps)}
+                avg of last 100 = ${Math.round(mean)}
+                min of last 100 = ${Math.round(min)}
+                max of last 100 = ${Math.round(max)}`.trim();
+    }
 
     const play = () => {
         play_pause.textContent = "Pause";
@@ -38,6 +72,7 @@ import { onMount } from 'svelte';
     })
 
     const renderLoop = () => {
+        renderFPS();
         console.log(universe);
         pre.textContent = universe.render();
         universe.tick();
@@ -46,6 +81,6 @@ import { onMount } from 'svelte';
     }
 
 </script>
-
 <button bind:this={play_pause} on:click={pauseOrPlay}>Play</button>
+<div bind:this={framesPerSecond}></div>
 <pre bind:this={pre}></pre>
